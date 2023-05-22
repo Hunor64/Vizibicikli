94ebf586a328d6c554592f92769327b706853ae6
Nhunor06 <61747377+Nhunor06@users.noreply.github.com>
2023-05-22 12:10:12 +0200

    Commit 12:10

--- vizibicikli/Program.cs
+++ vizibicikli/Program.cs
@@ -11,52 +11,7 @@ namespace vizibicikli
         static void Main(string[] args)
         {
             string fileLocation = "DataSource\\kolcsonzesek.txt";
-            /*
-            //Hagyományos
-            StreamReader sr = new StreamReader(fileLocation);
-            sr.ReadLine();
-            while (!sr.EndOfStream)
-            {
-                var mezok = sr.ReadLine().Split(';');
-                Kolcsonzes uj = new Kolcsonzes(mezok[0],
-                                               mezok[1][0],
-                                               int.Parse(mezok[2]),
-                                               int.Parse(mezok[3]),
-                                               int.Parse(mezok[4]),
-                                               int.Parse(mezok[5]));
-            }
-            sr.Close();
 
-            //ReadAllLines + Foreach, itt bontom fel
-            List<Kolcsonzes> megMasikMegintLista = new List<Kolcsonzes>();
-            foreach (var sor in File.ReadAllLines(fileLocation))
-            {
-                var ujMezok = sor.Split(';');
-                Kolcsonzes uj = new Kolcsonzes(ujMezok[0],
-                                               ujMezok[1][0],
-                                               int.Parse(ujMezok[2]),
-                                               int.Parse(ujMezok[3]),
-                                               int.Parse(ujMezok[4]),
-                                               int.Parse(ujMezok[5]));
-            }
-
-            //LINQ
-            List<Kolcsonzes> masikLista =
-            File.ReadAllLines(fileLocation)
-                .Skip(1)
-                .Select(x => new Kolcsonzes(x))
-                .ToList();
-
-            //5. záró
-            List<Kolcsonzes> megMasikMegintUjraLista = new List<Kolcsonzes>();
-            megMasikMegintUjraLista = File.ReadAllLines(fileLocation)
-                                          .Skip(1)
-                                          .Select(x => new Kolcsonzes(x))
-                                          .ToList();
-        */
-
-
-            //ReadAllLines + Foreach, konstruktorban bontom fel
             List<Kolcsonzes> megMasikLista = new List<Kolcsonzes>();
             foreach (var sor in File.ReadAllLines(fileLocation))
             {
