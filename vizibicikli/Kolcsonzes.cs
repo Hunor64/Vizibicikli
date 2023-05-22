47445636cbe7f674cb7c356dcb4c53dbb4f0456f
Nhunor06 <61747377+Nhunor06@users.noreply.github.com>
2023-05-22 12:45:12 +0200

    Final Commit 12:45

--- vizibicikli/Kolcsonzes.cs
+++ vizibicikli/Kolcsonzes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace vizibicikli
@@ -13,6 +14,10 @@ namespace vizibicikli
         int vOra;
         int vPerc;
 
+        public int idoHossz()
+        {
+            return vOra * 60 + VPerc - (EOra * 60 + EPerc);
+        }
         public Kolcsonzes(string csvSor) 
         {
             var mezok = csvSor.Split(';');
--- vizibicikli/Program.cs
+++ vizibicikli/Program.cs
@@ -13,10 +13,35 @@ namespace vizibicikli
             string fileLocation = "DataSource\\kolcsonzesek.txt";
 
             List<Kolcsonzes> megMasikLista = new List<Kolcsonzes>();
+            int index = 0;
             foreach (var sor in File.ReadAllLines(fileLocation))
             {
-                megMasikLista.Add(new Kolcsonzes(sor));
+                if (index != 0){megMasikLista.Add(new Kolcsonzes(sor));}index++;
             }
+            Console.WriteLine("5. Feladat: Napi kölcsönzések száma:",kolcsonzesek.Count);
+
+            Console.Write("6. Feladat: Kérek egy nevet: ");
+            string nev = "Kata";
+            Console.WriteLine(nev);
+            if (kolcsonzesek.Count(x => x.Nev == nev)==0)
+            {
+                Console.WriteLine("Nem volt ilyen nevű kölcsönző!");
+            }
+            else
+            {
+                Console.WriteLine($"\t{nev} kölcsönzései:");
+                foreach (var aktKolcsonzes in kolcsonzesek.Where(x => x.Nev == nev))
+                {
+                    Console.WriteLine($"\t{aktKolcsonzes.EOra}:{aktKolcsonzes.EPerc} - {aktKolcsonzes.VOra}:{aktKolcsonzes.VPerc}");
+                }
+                
+            }
+            //8.
+            Console.WriteLine("8. Feladat: A napi bevétel:", kolcsonzesek.Sum(ob => ob.idoHossz()) / 30 + 1);
+
+            //10.
+            kolcsonzesek.GroupBy(x => x.Jazon).ToList().ForEach(x => Console.WriteLine($"{x.Key} - {x.Count()}"));
+            
         }
     }
 }
